5bf0b38031fbb12fb2c0d19f0a44416eaab4614f
root <root@dltsv100-worker-wgwbnd.westeurope.cloudapp.azure.com>
2019-05-22 18:14:57 +0000

    asdad

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ import EC_Adam
 from models.resnet import ResNet50
 from mycompress import *
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "2" # GPU ID
+# os.environ["CUDA_VISIBLE_DEVICES"] = "2" # GPU ID
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.01, type=float, help='learning rate')
-parser.add_argument('--resume', '-r', default= True, action='store_true', help='resume from checkpoint')
+parser.add_argument('--resume', '-r', default= False, action='store_true', help='resume from checkpoint')
 args = parser.parse_args()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -53,7 +53,7 @@ train_sampler = torch.utils.data.SequentialSampler(trainset)
 # n_valid_examples = len(trainset) - n_train_examples
 #
 # trainset, _ = torch.utils.data.random_split(trainset, [n_train_examples, n_valid_examples])
-trainloader = torch.utils.data.DataLoader(trainset, sampler = train_sampler , batch_size=250, shuffle=False, num_workers=1)
+trainloader = torch.utils.data.DataLoader(trainset, sampler = train_sampler , batch_size=600, shuffle=False, num_workers=1)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
@@ -65,7 +65,7 @@ print('==> Building model..')
 # net = VGG('VGG19')
 net = ResNet50()
 
-
+net = nn.DataParallel(net,device_ids =[0,1])
 
 
 # net = PreActResNet18()
@@ -153,8 +153,6 @@ def test(epoch):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
     # Save checkpoint.
     acc = 100.*correct/total
@@ -176,5 +174,5 @@ for para in net.parameters():
 
 for epoch in range(start_epoch, start_epoch+200):
     train(epoch)
-    # test(epoch)
+    test(epoch)
 
